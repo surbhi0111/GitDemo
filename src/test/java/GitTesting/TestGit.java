012bf73c426e59041ce89a2de2a4f5750742eed3
surbhi <learnautomation0111@gmail.com>
2024-05-15 15:40:19 +0530

    this is commit from IST

---
+++ src/test/java/GitTesting/TestGit.java
@@ -0,0 +1,11 @@
+package GitTesting;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this is my first single commit");
+
+	}
+
+}
