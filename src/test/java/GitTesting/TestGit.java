c49d2cd05cdf32a8b1f793c857e02e19d62667a4
surbhi <learnautomation0111@gmail.com>
2024-05-15 16:00:49 +0530

    from GitStuff

--- src/test/java/GitTesting/TestGit.java
+++ src/test/java/GitTesting/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi this is my first single commit");
+		System.out.println("Hi this is from git stuff");
 
 	}
 
